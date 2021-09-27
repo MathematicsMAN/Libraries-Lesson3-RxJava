c27e9a30a78b0501632224b3abb476196787de84
MathematicsMAN <damir_a_z@mail.ru>
2021-09-27 13:50:49 +0300

    HomeWork Lesson4 Convert JPG to PNG

---
+++ app/src/main/java/ru/android/libraries_lesson3_rxjava/ConvertModel.kt
@@ -0,0 +1,6 @@
+package ru.android.libraries_lesson3_rxjava
+
+class ConvertModel {
+    var pathImageConverted: String? = null
+
+}
---
+++ app/src/main/java/ru/android/libraries_lesson3_rxjava/MainView.kt
@@ -0,0 +1,8 @@
+package ru.android.libraries_lesson3_rxjava
+
+import android.graphics.Bitmap
+
+interface MainView {
+
+    fun setPNGImage(bitmapPNG: Bitmap, pathPNG: String)
+}
